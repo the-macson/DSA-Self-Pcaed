3e01be8e23499f41c96808afabd4776c64d78aec
the-macson <ak9991927952@gmail.com>
2022-05-24 15:18:08 +0530

    added PowerSet

---
+++ Bit Magic/Learn/PowerSet.cpp
@@ -0,0 +1,21 @@
+// Power Set using Bitwise Operator
+#include<bits/stdc++.h>
+using namespace std;
+void printPowerSet(string str){
+    int n = str.length();
+    int powSize = pow(2,n);
+    for(int counter = 0; counter < powSize ; counter++){
+        for (int j = 0; j < n; j++){
+            if((counter & (1 << j)) != 0){
+                cout << str[j];
+            }
+        }
+        cout << endl;
+    }
+}
+int main(){
+    string str = "abc";
+    printPowerSet(str);
+}
+
+// Time complexity of this program is Theta(2^n * n)
